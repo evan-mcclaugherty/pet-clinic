5278a9ab429a8049fd468b071cf3bb115bd2ce4e
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-11-10 19:05:38 -0700

    List owners on web page. closes #16.

--- pet-clinic-web/src/main/java/evnmck/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/evnmck/springframework/petclinic/controllers/OwnerController.java
@@ -1,7 +1,5 @@
 package evnmck.springframework.petclinic.controllers;
 
-import evnmck.springframework.petclinic.model.Owner;
-import evnmck.springframework.petclinic.services.CrudService;
 import evnmck.springframework.petclinic.services.mapService.OwnerMapService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    private final CrudService<Owner, Long> crudService;
+    private final OwnerMapService ownerMapService;
 
-    public OwnerController() {
-        crudService = new OwnerMapService();
+    public OwnerController(OwnerMapService ownerMapService) {
+        this.ownerMapService = ownerMapService;
     }
 
     @GetMapping({"", "/", "/index", "index.html"})
     public String listPetOwners(Model model) {
-        model.addAttribute("owners", this.crudService.findAll());
+        model.addAttribute("owners", this.ownerMapService.findAll());
         return "owners/index";
     }
 }
