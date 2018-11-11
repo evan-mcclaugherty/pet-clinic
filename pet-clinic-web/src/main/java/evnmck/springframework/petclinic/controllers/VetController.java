d981571a3529f51a4c75623db5ea9157a4358a03
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-11-10 19:12:57 -0700

    List vet on web page. closes #15.

--- pet-clinic-web/src/main/java/evnmck/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/evnmck/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,21 @@
 package evnmck.springframework.petclinic.controllers;
 
+import evnmck.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
-    @GetMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping({"/vets", "/vets/index.html", "/vets/index"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", this.vetService.findAll());
         return "vets/index";
     }
 }
